724a379e4a9ad1b028ce703d08b900d05c48e4f1
Thiago Chiarelotto <thiago.chiarelotto@gmail.com>
2017-03-16 00:03:59 -0300

    Alteração para salvar dados cadastrais no Firebase

--- app/src/main/java/i_home/studio/com/i_home/CadastarActivity.java
+++ app/src/main/java/i_home/studio/com/i_home/CadastarActivity.java
@@ -2,43 +2,78 @@ package i_home.studio.com.i_home;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import i_home.studio.com.i_home.Config.ConfiguracaoFirebase;
 
 public class CadastarActivity extends Activity {
 
 
-    private Button botaoCadastro;
+    private FirebaseAuth firebaseAuth;
+    private Button botaoCadastrar;
+    private EditText nome;
+    private EditText email;
+    private EditText senha;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastar);
 
+        nome = (EditText) findViewById(R.id.et_nome);
+        email = (EditText) findViewById(R.id.et_email_Cadastro);
+        senha = (EditText) findViewById(R.id.et_Senha_Cadastro);
 
-        botaoCadastro = (Button) findViewById(R.id.bt_Cadastro);
+        botaoCadastrar = (Button) findViewById(R.id.bt_Cadastrar);
 
-        botaoCadastro.setOnClickListener(new View.OnClickListener() {
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
-
             public void onClick(View v) {
 
-                 DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference();
-                 DatabaseReference usuáriosReferencia = databaseReferencia.child("Usuários");
-
-                Usuario usuario = new Usuario();
-
-                usuario.getEmail() = .getText().toString(); ;
-
-                usuáriosReferencia.child("001").child("Nome").setValue("Bolacha");
+                usuario = new Usuario();
+                usuario.setNome(nome.getText().toString());
+                usuario.setNome(email.getText().toString());
+                usuario.setNome(senha.getText().toString());
+                cadastrarUsuario();
 
             }
         });
 
-
-
     }
+
+    private void cadastrarUsuario(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(CadastarActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                    Toast.makeText(CadastarActivity.this,"Sucesso ao cadastrar usuário", Toast.LENGTH_LONG).show();
+
+                }else{
+
+                    Toast.makeText(CadastarActivity.this,"Erro ao cadastrar usuário", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/i_home/studio/com/i_home/MainActivity.java
+++ app/src/main/java/i_home/studio/com/i_home/MainActivity.java
@@ -2,31 +2,52 @@ package i_home.studio.com.i_home;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends Activity {
 
     private Button botaoCadastrar;
+    private Button botaoLogin;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoCadastrar = (Button) findViewById(R.id.bt_Cadastrar);
+        botaoLogin = (Button) findViewById(R.id.bt_Login);
+
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this,Quarto.class));
+
+            }
+        });
+
+       botaoCadastrar = (Button) findViewById(R.id.bt_Cadastrar);
 
           botaoCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 startActivity(new Intent(MainActivity.this,CadastarActivity.class));
+
 
             }
         });
